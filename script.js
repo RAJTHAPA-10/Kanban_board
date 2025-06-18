33bb9984de7076def9b139352432e6d69cc623ad
RAJ THAPA <rajrudrathapa@gmail.com>
2025-06-19 02:39:56 +0530

    drag and drop feature impelemented

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+let draggedelement=null;
 function addtask(columnid)
 {
     const input = document.getElementById(`${columnid}-input`);
@@ -17,6 +18,33 @@ function getcosmadecard(inputvalue)
     const inputcard=document.createElement("div");
     inputcard.innerText=inputvalue;
     inputcard.classList.add("card");
+    inputcard.draggable=true;
+    inputcard.addEventListener("dragstart",funstart)
+    inputcard.addEventListener("dragend",funend)
     return inputcard;
 
+}
+
+function funstart()
+{
+    this.classList.add("dragable");
+    draggedelement=this;
+
+}
+function funend()
+{
+    this.classList.remove("dragable");
+}
+
+const columns=document.querySelectorAll(".column .tasks");
+columns.forEach((elements)=>
+{
+    elements.addEventListener("dragover",funover)
+})
+
+function funover(event)
+{
+    event.preventDefault();
+    this.appendChild(draggedelement);
+    
 }
